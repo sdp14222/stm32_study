5226088f896cd32f3b06fa6b5409ab46117011aa
SangDon Park <sdp14222@gmail.com>
2022-03-30 15:53:42 +0900

    Add HAL_TIM_PeriodElapsedCallback

--- STM32F103C8T6_workspace/4. TIM_Interrupt/Core/Src/main.c
+++ STM32F103C8T6_workspace/4. TIM_Interrupt/Core/Src/main.c
@@ -156,7 +156,13 @@ static void MX_NVIC_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+	if(htim->Instance == TIM2)
+	{
 
+	}
+}
 /* USER CODE END 4 */
 
 /**
